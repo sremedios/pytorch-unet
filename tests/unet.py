be9dd530b9862d520a981a7bfa58f9a3ca9d4da6
Shuo Han <shan50@jhu.edu>
2021-06-02 21:45:11 -0400

    Remove pytorch layers

--- tests/unet.py
+++ tests/unet.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
   
 import torch
 from pytorch_unet import UNet
-from pytorch_layers import Config
-from torchviz import make_dot
+from pytorchviz import make_dot
+
+class Wrapper(torch.nn.Module):
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+    def forward(self, x):
+        y = self.net(x)
+        if isinstance(y, dict):
+            y = tuple(y.values())
+        return y
 
 
-Config.dim = 2
-Config.padding_mode = 'reflect'
-Config.show()
-
-unet = UNet(1, 4, 3, 8, output_levels=[3, 0, 1])
+x = torch.rand(1, 1, 16, 16, 16).cuda().float()
+unet = UNet(1, 4, 3, 8, output_levels=[3, 0, 1]).cuda()
+unet = Wrapper(unet)
 print(unet)
-
-# x = torch.rand(1, 1, 16, 16, 16)
-x = torch.rand(1, 1, 16, 16)
 y = unet(x)
-if isinstance(y, dict):
-    for k, v in y.items():
-        print(k, v.shape)
-    outputs = list()
-    for v in y.values():
-        outputs.append(torch.nn.functional.interpolate(v, size=x.shape[2:]))
-    output = sum(outputs)
-else:
-    print(y.shape)
-    output = y
-dot = make_dot(output, params=dict(unet.named_parameters()))
-dot.format = 'svg'
+
+dot = make_dot(x, unet)
+dot.format = 'pdf'
 dot.render('unet')
